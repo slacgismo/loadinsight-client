be6075cd5f88c219180e86debcf704e195c3b838
Peuan Thinsan <peuanthinsan@gmail.com>
2020-06-09 18:34:50 -0700

    add StyledHeader and StyledTitle

---
+++ src/styles/app.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.div`
+  width: 100%;
+  height: 78px;
+  background-color: #fff;
+  border-bottom: 1px solid #D2D2D2;
+  padding: 22px 15px 0px 15px;
+`;
+
+export const StyledTitle = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  font-weight: 700;
+  font-size: 28px;
+  color: #404040;
+`;
