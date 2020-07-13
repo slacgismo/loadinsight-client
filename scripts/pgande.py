189bffad4dae5edef62c878542e8a6bb08de9803
Kevin Rohling <kevin@kevinrohling.com>
2020-07-13 12:04:08 -0700

    automatically pick the start/end dates when running the 'download-pge' script, also do not save files for days that do not have any pge data available

--- scripts/pgande.py
+++ scripts/pgande.py
@@ -6,11 +6,6 @@ import pandas as pd
 from datetime import datetime, timedelta
 import numpy as np
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--start')
-parser.add_argument('--end')
-args = parser.parse_args()
-
 def get_load_profile(date):
     buffer = BytesIO()
     c = pycurl.Curl()
@@ -20,7 +15,7 @@ def get_load_profile(date):
     body = c.perform_rs()
     if c.getinfo(pycurl.HTTP_CODE) != 200:
         print(f"ERROR: get_load_profile(date={date}): no data")
-        return pd.DataFrame()
+        return pd.DataFrame(), False
     c.close()
 
     df = pd.read_csv(StringIO(body)).dropna(how='all').transpose()
@@ -38,7 +33,7 @@ def get_load_profile(date):
         return datetime(y,m,d,H,M,0)
     df.index = list(map(lambda t: get_time(datename,t),df.index))
 
-    return df
+    return df, True
 
 def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days+1)):
@@ -49,24 +44,19 @@ def get_loads(start,stop,date_format='%m/%d/%y',show_progress=False):
         start = datetime.strptime(start,date_format)
     if type(stop) is str:
         stop = datetime.strptime(stop,date_format)
-    res1 = pd.DataFrame()
+    
     for date in daterange(start,stop):
         datename = date.strftime('%Y%m%d')
         if show_progress:
             print(f"Processing {date}...",flush=True)
         try:
-            df = get_load_profile(date)
-            df.to_csv(f'public/api/pge/{datename}.csv')
+            df, success = get_load_profile(date)
+            if success:
+                df.to_csv(f'public/api/pge/{datename}.csv')
         except Exception as err:
             print(f"ERROR: get_load_profile(date={date}): {err}")
-            df = pd.DataFrame()
-        try:
-            res1 = res1.append(df,sort=True)
-        except Exception as err:
-            savefile = date.strftime('%Y%m%d-err.csv')
-            print(f"ERROR: get_load_profile(date={date}): {err}, saving bad data to {savefile}")
-            df.to_csv(savefile)
-            pass
-    return res1
 
-get_loads(args.start, args.end,show_progress=True)
+start = datetime.strptime('1/1/20', '%m/%d/%y')
+end = datetime.now()
+
+get_loads(start, end,show_progress=True)
